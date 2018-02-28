106f677e9bcaaea7ee17c5fddcd62ce602572688
Steven Phillips <ninjamonkeys707@gmail.com>
2018-02-28 03:02:33 -0600

    adding database connections

--- includes/db.php
+++ includes/db.php
@@ -14,3 +14,28 @@
 // Make sure to include support for DB_PORT. See the
 // PHP Documentation for the MySQL PDO DSN:
 // http://php.net/manual/en/ref.pdo-mysql.connection.php
+
+
+//connecting to the database
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$name = getenv('DB_DATABASE');
+$dsn = "mysql:host=$host;port=$port;dbname=$name";
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+
+try{
+    $conn = new PDO($dsn, $username, $password);
+}catch(PDOEception $e){
+    $error_message = $e->getMessage();
+    include('db_error.php');
+    exit();
+}
+// var_dump($dsn);
+// die();
+
+
+
+
+?>  
+
--- index.php
+++ index.php
@@ -7,11 +7,76 @@
 require_once('./includes/init.php');
 
 // Here you might connect to the database and show off some of your newest guitars.
+require_once('./includes/db.php');
+
+    $category_id = filter_input(INPUT_GET, 'category_id', FILTER_VALIDATE_INT);
+    if($category_id == null || $category_id == false){
+        $category_id = 1;
+    }
+
+    //category
+    $query = 'SELECT * FROM categories WHERE categoryID = :category_id';
+    $statement = $conn->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->execute();
+    $category = $statement->fetch();
+    $category_name = $category['categoryName'];
+    $statement->closeCursor();
+
+    // var_dump($host);
+    // die();
+
+    //categories    
+    $query = 'SELECT * FROM categories ORDER BY categoryID';
+    $statement = $conn->prepare($query);
+    // $statement->bindValue(':category_id', $category_id);
+    $statement->execute();
+    $categories = $statement->fetchAll();
+    $statement->closeCursor();
+
+    // var_dump($categories);
+    // die();
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>The Pick of Destiny</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="*" />
+</head>
+<body>
+<div class="container">
+<h1>Welcome to The Pick of Destiny</h1>
+        <div class="row">
+            <div class="col-sm-3">
+                <h2>Categories</h2>
+                <nav>
+                    <ul>
+                        <?php foreach($categories as $category) : ?>
+                        <li>
+                            <a href="?category_id=<?php echo $category['categoryID']; ?>">
+                                <?php echo $category['categoryName'];?>
+                            </a>
+                        </li>
+                        <?php endforeach?>
+                    </ul>
+                </nav>
+            </div>
+        
 
-This is my super awesome Guitar Shop!
+            <div class="col-sm-6">
+                
+            </div>
+            <div class="col"></div>
+        </div>
+</div>
 
-- All Orders
-- Unshipped Orders
-- Products
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
